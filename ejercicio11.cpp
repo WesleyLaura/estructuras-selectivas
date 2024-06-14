6ade35019da1d35cc60059598326aff984868576
Wesley Laura <wrlaurac@unjbg.edu.pe>
2024-06-13 22:05:00 -0500

    En el ejercicio11 de control selectiva actualizando las variables, una librería que me permite utilizar otros caracteres que no están en el conjunto de caracteres ASCII, cambiando el primer texto de entrada, añadiendo dos condicionales más al código, añadiendo dos penultimas de textod de salida

--- ejercicio11.cpp
+++ ejercicio11.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <windows.h>
+
 using namespace std;
 
 int main() {
-    int q, r, s;
-    cout << "Ingrese el primer numero (q): ";
-    cin >> q;
-    cout << "Ingrese el segundo numero (r): ";
-    cin >> r;
-    cout << "Ingrese el tercer numero (s): ";
-    cin >> s;
+	SetConsoleOutputCP(CP_UTF8);
+    int numero1, numero2, numero3;
+    cout << "Ingrese el numero a vereficar si es múltilplo de los dos siguientes numeros a ingresar (x°): ";
+    cin >> numero1;
+    cout << "Ingrese el segundo numero (y): ";
+    cin >> numero2;
+    cout << "Ingrese el tercer numero (z): ";
+    cin >> numero3;
 
-    if (q % r == 0 && q % s == 0) {
-        cout << "El numero " << q << " es multiplo de " << r << " y " << s << "." << endl;
-    } else {
-        cout << "El numero " << q << " no es multiplo de " << r << " y " << s << "." << endl;
+    if (numero1 % numero2 == 0 && numero1 % numero3 == 0) {
+        cout << "El numero " << numero1 << " es múltiplo de " << numero2 << " y " << numero3 << "." << endl;
+    } else if (numero1 % numero2 != 0 && numero1 % numero3 == 0){
+    	cout << "El numero " << numero1 << " no es múltiplo de " << numero2 << " y " << numero3 << " si es múltiplo de "<<numero1<<"." << endl; 
+	} else if (numero1 % numero2 == 0 && numero1 % numero3 != 0){
+		cout << "El numero " << numero1 << "  es múltiplo de " << numero2 << " y " << numero3 << "no es múltiplo de "<<numero1<<"." << endl; 
+	}
+	else {
+        cout << "El numero " << numero1 << " no es multiplo de " << numero2 << " y " << numero3 << "." << endl;
     }
 
     return 0;
