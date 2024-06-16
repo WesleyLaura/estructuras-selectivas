423b5a47652a066ba62878782f4da2a434dd6780
Wesley Laura <wrlaurac@unjbg.edu.pe>
2024-06-15 22:41:30 -0500

    Actualizando ejercicio2 de estructura selectivas, añadiendo una condicional para vereficar si la entrada es negativa

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -6,14 +6,22 @@ int main() {
 	float pagofinal;
 	cout<<"Ingrese la cantidad de la compra realizada: ";
 	cin >> comprarealizada;
-	if (comprarealizada>1000) {
+	if (comprarealizada>=0){
+		if (comprarealizada>1000) {
 		pagofinal = comprarealizada*0.80;
 		cout<<"Su pago final es de: " << pagofinal << endl;
 	
 	}else{
 		cout<<"Su compra no alcanzo los estandares para aplicar el descuento:("<<endl;
+		pagofinal=comprarealizada;
+		cout<<pagofinal<<endl;
 		cout<<"Vuelva otro dia y compre mas productos: "<<endl;
 	}
+		
+	} else{
+		cout<<"INGRESASTE UN NUMERO NEGATIVO";
+	}
+	
 	return 0;
 }
 
