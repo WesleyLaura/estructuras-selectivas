091db2988aa0553b7f13854be7e0c19d5dc7b327
Wesley Laura <wrlaurac@unjbg.edu.pe>
2024-06-18 12:20:51 -0500

    Actualizando ejercicio5 de estructuras selectivas: Añadiendo una librería que permite de salida carácteres especiales

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
-int main() {
+int main() { 
     int edad;
     char sexo;
     float numPulsaciones;
-
+	SetConsoleOutputCP(CP_UTF8);
     cout << "Ingrese su edad: ";
     cin >> edad;
     cout << "Ingrese su sexo (M para masculino, F para femenino): ";
     cin >> sexo;
 
-    sexo = toupper(sexo);
-
     if (sexo == 'F') {
-        numPulsaciones = (220 - edad) / 10.0;
+        numPulsaciones = (220 - edad) / 10;
     } else if (sexo == 'M') {
-        numPulsaciones = (210 - edad) / 10.0;
+        numPulsaciones = (210 - edad) / 10;
     } else {
-        cout << "Sexo no valido. Por favor, ingrese M o F." << endl;
-        return 1; 
+        cout << "Sexo no válido " << endl;
+        return 0; 
     }
 
-    cout << "El numero de pulsaciones por cada 10 segundos de ejercicio aerobico es: " << numPulsaciones << endl;
+    cout << "El número de pulsaciones por cada 10 segundos de ejercicio aeróbico es: " << numPulsaciones << endl;
 
     return 0;
 }
